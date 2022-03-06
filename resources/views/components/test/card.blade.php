368168a7c36ef3545fabf1c96618ef2451e30d96
YukiKuramoto <otomaru.kcufs@i.softbank.jp>
2022-03-07 02:58:36 +0900

    コンポーネントにprops設定

--- resources/views/components/test/card.blade.php
+++ resources/views/components/test/card.blade.php
@@ -1,3 +1,9 @@
+@props([
+  'title',
+  'content' => '初期値本文です',
+  'message' => 'メッセージ初期値です',
+])
+
 <div class="border-2 shadow-md w-1/4 p-2">
   <div>{{ $title }}</div>
   <div>画像</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,4 +2,5 @@
   <x-slot name=header>ヘッダー１</x-slot>
   コンポーネントテスト１
   <x-test.card title="タイトルテスト1" content="コンテンツテスト1" :message="$message" />
+  <x-test.card title="タイトルテスト2" />
 </x-test.app>
